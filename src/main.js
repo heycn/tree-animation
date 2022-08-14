8d65692b2576e3d47c86a6263eb5adb0d0719d45
heycn <x1chennan@qq.com>
2022-08-14 18:16:01 +0800

    style

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-let canvas = document.getElementById('canvas')
-let ctx = canvas.getContext('2d')
+const canvas = document.getElementById('canvas'),
+      ctx = canvas.getContext('2d')
 
 let angleIncrement = (30 * Math.PI) / 180
-let startX = canvas.width / 2
-let startY = canvas.height - 120
-let height = (canvas.height * 7) / 24
 
-let thickness = 2
-let maxDepth = 8
-let count = 0
-let branchPropagation = 5
+const startX = canvas.width / 2,
+      startY = canvas.height - 120,
+      height = (canvas.height * 7) / 24
+
+let thickness = 2,
+    maxDepth = 8,
+    count = 0,
+    branchPropagation = 5,
+    color = '#fff'
+
 let createRect = (x, y, width, height, color) => {
   ctx.fillStyle = color
   ctx.fillRect(x, y, width, height)
@@ -27,21 +30,19 @@ let drawLine = (x1, y1, x2, y2, thickness, color) => {
 
 let drawBranch = (x, y, height, thickness, angle, depth) => {
   if (depth > maxDepth) return
-  let endX = x - height * Math.sin(angle)
-  let endY = y - height * Math.cos(angle)
 
-  drawLine(x, y, endX, endY, thickness, '#f60')
+  let endX = x - height * Math.sin(angle),
+      endY = y - height * Math.cos(angle),
+      newHeight = (height * 8) / 12,
+      newThickness = (thickness * 2) / 3,
+      angleStart
 
-  let newHeight = (height * 8) / 12
-  let newThickness = (thickness * 2) / 3
-  let angleStart
+  drawLine(x, y, endX, endY, thickness, color)
 
-  angleStart =
-    branchPropagation % 2 === 0
-      ? angle -
-        angleIncrement / 2 -
-        (Math.trunc(branchPropagation / 2) - 1) * angleIncrement
-      : angle - Math.trunc(branchPropagation / 2) * angleIncrement
+
+  angleStart = branchPropagation % 2 === 0
+                ? angle - angleIncrement / 2 - (Math.trunc(branchPropagation / 2) - 1) * angleIncrement
+                : angle - Math.trunc(branchPropagation / 2) * angleIncrement
 
   for (let i = 0; i < branchPropagation; i++) {
     drawBranch(
@@ -56,7 +57,7 @@ let drawBranch = (x, y, height, thickness, angle, depth) => {
 }
 
 let drawTree = () => {
-  createRect(0, 0, canvas.width, canvas.height, '#000')
+  createRect(0, 0, canvas.width, canvas.height, '#1b1b1b')
   angleIncrement -= 0.02
   drawBranch(startX, startY, height, thickness, 0, Math.PI / 2)
   requestAnimationFrame(drawTree)
